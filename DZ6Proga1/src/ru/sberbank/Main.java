9dd6e22b5181ca5f931e3fd744d8d9ec057dbcb2
DmitrySM7 <55131328+DmitrySM7@users.noreply.github.com>
2019-11-01 14:10:51 +0300

    Add files via upload

---
+++ DZ6Proga1/src/ru/sberbank/Main.java
@@ -0,0 +1,26 @@
+package ru.sberbank;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Task<Integer> task = new Task<>(()-> {
+           int result = 0;
+           int x = 2500;
+           int y = 3;
+           for (int i = 0; i < x; i++) {
+               result+=y;
+           }
+           return  result;
+        });
+
+        for (int i = 0;i<20;i++) {
+            new Thread(()->{
+                try {
+                    System.out.println(task.get());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
